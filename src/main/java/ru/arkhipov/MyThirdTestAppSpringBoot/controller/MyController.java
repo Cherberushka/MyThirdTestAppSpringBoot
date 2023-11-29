137a186f889d1b745643c407908c92160f64b8fc
Cherberushka <kluev.ekb@mail.ru>
2023-11-30 00:41:51 +0500

    Initial commit

---
+++ src/main/java/ru/arkhipov/MyThirdTestAppSpringBoot/controller/MyController.java
@@ -0,0 +1,72 @@
+package ru.arkhipov.MyThirdTestAppSpringBoot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.arkhipov.MyThirdTestAppSpringBoot.exception.ValidationFailedException;
+import ru.arkhipov.MyThirdTestAppSpringBoot.model.*;
+import ru.arkhipov.MyThirdTestAppSpringBoot.service.ModifyResponseService;
+import ru.arkhipov.MyThirdTestAppSpringBoot.service.ValidationService;
+import ru.arkhipov.MyThirdTestAppSpringBoot.util.DateTimeUtil;
+
+import javax.validation.Valid;
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class MyController {
+    private final ValidationService validationService;
+    private final ModifyResponseService modifyResponseService;
+
+    @Autowired
+    public MyController(ValidationService validationService, @Qualifier("ModifySystemTimeResponseService") ModifyResponseService modifyResponseService) {
+        this.validationService = validationService;
+        this.modifyResponseService = modifyResponseService;
+    }
+
+    long startTime = System.currentTimeMillis();
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@Valid @RequestBody Request request, BindingResult bindingResult) {
+        log.info("Получен запрос обратной связи: {}", request);
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(DateTimeUtil.getCustomFormat().format(new Date()))
+                .code(Codes.SUCCESS)
+                .errorCode(ErrorCodes.EMPTY)
+                .errorMessage(ErrorMessages.EMPTY)
+                .build();
+
+        try {
+            validationService.isValid(bindingResult);
+        } catch (ValidationFailedException e) {
+            log.error("Ошибка валидации: {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.VALIDATION_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.VALIDATION);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            log.error("Произошла неизвестная ошибка: {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.UNKNOWN_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.UNKNOWN);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        modifyResponseService.modify(response);
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        log.info("Время выполнения в миллисекундах: {}", executionTime);
+        log.info("Ответ: {}", response);
+        return new ResponseEntity<>(modifyResponseService.modify(response), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/ru/arkhipov/MyThirdTestAppSpringBoot/exception/UnsupportedCodeException.java
@@ -0,0 +1,9 @@
+package ru.arkhipov.MyThirdTestAppSpringBoot.exception;
+
+public class UnsupportedCodeException extends RuntimeException {
+    public UnsupportedCodeException(String message) {
+
+        super(message);
+    }
+}
+
